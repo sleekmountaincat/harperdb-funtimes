e15738c033658748b48e25186605cc971c024d87
xris <chris.nelson@campspot.com>
2025-05-28 13:02:24 -0600

    add primary key

--- scripts/wait-for-harperdb.sh
+++ scripts/wait-for-harperdb.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# wait for harper to come up.
+# make sure to include timeout in steps that use this script
+
 echo "waiting for harperdb operations api..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9925)" != "200" ]]; do
   sleep .5;
